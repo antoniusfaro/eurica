618b21ae9622ae85d4a2e10b0b030d39296429bc
antoniusfaro <antoniusf44@gmail.com>
2022-05-25 13:15:04 +0700

    add: scan, scan result, item list, detail item, saved item list, detail saved item activities

---
+++ app/src/main/java/com/bangkit/eurica/DetailSavedActivity.kt
@@ -0,0 +1,11 @@
+package com.bangkit.eurica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailSavedActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_saved)
+    }
+}
